318082af232935740537c785f45447d5cea4ac85
Yoshenan <yosh45650@gmail.com>
2025-12-30 22:08:24 +0800

    new changes in fine

--- Fine.java
+++ Fine.java
@@ -1,9 +1,11 @@
 
-public class fine {
- private Vehicle vehicle;
- private Parking parking;
+public class Fine {
+ public  Vehicle vehicle;
+ public  Parking parking;
+ public boolean isParked;
+  
 
-    public fine(){
+    public Fine(){
      this.vehicle = new Vehicle();
      this.parking = new Parking();
      vehicle.inputPlateNumber();
@@ -13,29 +15,41 @@ public class fine {
     }
 
     public class fineType{
-        void finePayType(){
-         double time = vehicle.duration;
-         if(time > 24.00){
-           totalRate += 50;
-         }
+        double  finePayType(double totalRate, double duration){
+         if(duration > 24.00 || isParked == false){
+           System.out.println("Your no plate has fine : " + vehicle.plateNumber);
         }
+        return totalRate;
     }
    
-    class fixed extends fineType{
-        void finePayType(){
-
+    class fixed extends fineType{//fixed fine type
+        @Override
+        double finePayType(double totalRate , double duration){
+           return  totalRate + 50.00;
         }
     }
 
-    class progessive extends fineType{
-        void finePayType(){
+    class progessive extends fineType{//progessive fine type
+        @Override
+        double finePayType(double totalRate , double duration){
             
+            if(duration== 24.00){
+               return totalRate + 50;
+            }else if(duration> 24 && duration<48){
+               return totalRate + 50 + 100;
+            }else if (duration > 48 && duration <72){
+               return totalRate +50 + 150;
+            } else {
+                return totalRate+ 50 + 200;
+            }
         }
     }
 
-    class hourly extends fineType{
-        void finePayType(){
-            
+    class hourly extends fineType{//hourly fine type 
+        @Override
+        double finePayType(double totalRate , double duration){
+            return totalRate + (duration - 24) * 20;
         }
+    }
     }
 }
